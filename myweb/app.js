9bd84dddbcf3df9f7e00c2791857aec749c4e0b7
etff <wish2rich@gmail.com>
2018-04-07 03:59:42 +0900

    upload

--- myweb/app.js
+++ myweb/app.js
@@ -6,8 +6,20 @@ var express = require('express');
 var path = require('path');                     
 var cors = require('cors')
 var app = express();
-var router = require('./routes/index')
+var router = require('./routes/index');
+var multer = require('multer');
+app.use('/user', express.static('uploads'));
 
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+var upload = multer({ storage: _storage })
 var http = require('http');                     //???
 var createError = require('http-errors');       //???
 var cookieParser = require('cookie-parser');    //???
@@ -43,11 +55,14 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-app.post('/save.json', function (req, res) {
+//미들웨어가 먼저 실행
+app.post('/save.json', upload.single('file'), function (req, res) {
   var message = {};
   message.name = req.param('name');
   message.email = req.param('email');
   message.message = req.param('msg');
+  message.file = req.param('file');
+  
   save(message, res);
 });
 
@@ -138,107 +153,6 @@ function count(res, list) {
       res.send(resultObj);
   });
 }
-
-
-
-// 저장 
-// app.post('/save.json', function (req, res) {
-//   var message = {};
-//   message.name = req.param('name');
-//   message.email = req.param('email');
-//   message.message = req.param('msg');
-//   save(message, res);
-// });
-
-// // 상세보기
-// app.get('/view.json', function(req, res) {
-//   var id = req.param('id');
-//   view(id, res);
-// });
-
-// // 목록보기
-// app.get('/list.json', function(req, res) {
-//   var pageNo = req.param('pageNo');
-//   list(pageNo, res);
-// });
-
-// // 저장하기
-// function save(message, res) {
-//   var query = connection.query('INSERT INTO board SET ?', message, function (err, result) {
-//       var resultObj = {};
-//       if (err) {
-//           resultObj.success = false;
-//           console.log(err);
-//       } else {
-//           resultObj.success = true;
-//           resultObj.id = result.insertId;
-//       }
-//       res.send(JSON.stringify(resultObj));
-//   });
-//   console.log(query.sql);
-// }
-
-// // 상세보기
-// function view(id, res) {
-//   console.log(id);
-//   var query = connection.query('select * from board where id = ' +
-//                                connection.escape(id), function (err, result) {
-//       var resultObj = {};
-//       if (err) {
-//           resultObj.success = false;
-//           console.log(err);
-//       } else {
-//           res.json(result);
-//           return;
-//       }
-//       res.send(JSON.stringify(resultObj));
-//   });
-//   console.log(query.sql);
-// }
-
-
-// // 목록보기
-// function list(pageNo, res) {
-//   console.log(pageNo);
-//   var num = parseInt(pageNo) - 1;
-//   if (num < 0) {
-//       num = 0;
-//   }
-//   var query = connection.query('select * from board order by id desc limit ?, ?'
-//                                , [num * 3, 3]
-//                                , function (err, results) {
-//       var resultObj = {};
-//       if (err) {
-//           resultObj.success = false;
-//           console.log(err);
-//       } else {
-//           count(res, results);
-//           return;
-//       }
-//       res.send(resultObj);
-//   });
-//   console.log(query.sql);
-// }
-
-// function count(res, list) {
-//   var query = connection.query('select count(*) as count from board'
-//                                , function (err, results) {
-//       var resultObj = {};
-//       if (err) {
-//           resultObj.success = false;
-//           console.log(err);
-//       } else {
-//           resultObj.count = results[0].count;
-//           resultObj.perpage = 3;
-//           resultObj.list = list;
-//           res.json(resultObj);
-//           return;
-//       }
-//       res.send(resultObj);
-//   });
-// }
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myweb/routes/index.js
+++ myweb/routes/index.js
@@ -7,26 +7,6 @@ var main = require('./main/main')
 var email = require('./email/email')
 var join = require('./join/index')
 var board = require('./board/board')
-
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'devdb',
-    user: 'devuser',
-    password: 'devpass'
-});
-connection.connect();
-
-// request 파서
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-// router.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, '../public/main.html'))
-//   });
   
 router.get('/', defaultPage);
 router.get('/write', defaultPage);
